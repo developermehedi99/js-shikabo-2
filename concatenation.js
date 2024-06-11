4d07ed067664d9abf755787456a84a5b0755846e
Mehedi Hasan <mdmehedihasan20010@gmail.com>
2024-06-11 17:53:47 +0600

    third commit

--- concatenation.js
+++ concatenation.js
@@ -5,8 +5,9 @@ const fullName = fName + " " + sName;
 // console.log("full Name:", fullName);
 
 const number1 = 44;
-const number2 = "55.88";
-const number = parseFloat(number2);
+const number2 = "55.88243";
+let number = parseFloat(number2);
+number = number.toFixed(3);
 const total = number1 + number;
 
 console.log(total);
---
+++ conditaional.js
@@ -0,0 +1,8 @@
+const myBujet = 80;
+const kacchi = 600;
+
+if (myBujet > kacchi) {
+  console.log("uff kacchi khabo re");
+} else {
+  console.log("kichu khowya hobe na");
+}
