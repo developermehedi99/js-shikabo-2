eba6b765429c5dfad72151d568fb82c564752226
Mehedi Hasan <mdmehedihasan20010@gmail.com>
2024-06-11 17:30:58 +0600

    sec

--- concatenation.js
+++ concatenation.js
@@ -2,4 +2,11 @@ const fName = "Nurmoni";
 const sName = "original matha pagol";
 const fullName = fName + " " + sName;
 
-console.log(fullName);
+// console.log("full Name:", fullName);
+
+const number1 = 44;
+const number2 = "55.88";
+const number = parseFloat(number2);
+const total = number1 + number;
+
+console.log(total);
