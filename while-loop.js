a969c694ca37037c6be4cf322044a4b1b73aff9b
Mehedi Hasan <mdmehedihasan20010@gmail.com>
2024-06-12 20:03:55 +0600

    while-loop

--- while-loop.js
+++ while-loop.js
@@ -1,13 +1,26 @@
 let count = 0;
 
 while (count < 6) {
-  console.log("i j kobe vlo hote parbo");
-  console.log(count);
+  //   console.log("i j kobe vlo hote parbo");
+  //   console.log(count);
   count++;
 }
 
 let number = 1;
 while (number < 8) {
-  console.log(number);
+  //   console.log(number);
   number++;
+}
+
+let price = 10;
+while (price > 1) {
+  //   console.log(price);
+  price--;
+}
+
+const frind = ["mim", "nurmoni", "usaila", "liza", "tisa", "farzana", "others"];
+let num = 0;
+while (num < frind.length) {
+  console.log(frind[num]);
+  num++;
 }
