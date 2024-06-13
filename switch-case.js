c4366bb64a13aed52b1386d176e7362da63205d3
Mehedi Hasan <mdmehedihasan20010@gmail.com>
2024-06-13 10:57:55 +0600

    ternary

---
+++ switch-case.js
@@ -0,0 +1,32 @@
+let num = 0;
+
+switch (num) {
+  case 0:
+    console.log("zaro");
+    break;
+  case 1:
+    console.log("one");
+    break;
+  case 2:
+    console.log("two");
+    break;
+  case 3:
+    console.log("three");
+    break;
+  default:
+    console.log("default");
+}
+
+let age = 16;
+
+switch (true) {
+  case age >= 15 && age <= 17:
+    console.log("cholbe atai good");
+    break;
+  case age >= 18 && age <= 21:
+    console.log("good vlo");
+    break;
+  case age >= 22 && age <= 26:
+    console.log("ache avarage");
+    break;
+}
---
+++ ternary-oparetor.js
@@ -0,0 +1,3 @@
+let price = 20;
+let kacchi = price > 15 ? "kachhi khabo re" : "kachhi khowya hobe na";
+console.log(kacchi);
